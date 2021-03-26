1d3a399da86e58ebc71c3249aa1af7a7a629cd4e
Aram Lemmens <460546@student.fontys.nl>
2021-03-26 15:19:33 +0100

    [add] puzzel opdracht 1 en 2

---
+++ Algoritmiek puzzel 1/Algoritmiek puzzel 1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Algoritmiek_puzzel_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Number = Convert.ToInt32(textBox1.Text);
+            int sum = 0;
+
+            for (int i = 0;i < Number; i = i + 3)
+            {
+                //Console.WriteLine(i);
+                sum = sum + i;
+            }
+            for (int j = 0; j < Number; j = j + 5)
+            {
+                //Console.WriteLine(i);
+                sum = sum + j;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Algoritmiek puzzel 2/Algoritmiek puzzel 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Algoritmiek_puzzel_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 2;
+            int sum = 0;
+            while (i < 4000000 || j < 4000000)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine(j);
+                i = i + j;
+                j = j + i;
+                if (i%2 == 0)
+                {
+                    sum = sum + i;
+
+                }
+                if (j % 2 == 0)
+                {
+                    sum = sum + j;
+                }
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
